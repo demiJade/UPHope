959a97862dd39a53bd0a5f409b9b59df967c3db5
demijade <sy_judy@yahoo.com>
2016-07-17 16:11:03 +0800

    added news page

---
+++ news.php
@@ -0,0 +1,39 @@
+<?php global $query_string; // required
+/**
+* The template for displaying institutions.
+*
+* Template Name: Institutions
+* 
+* 
+*
+*/
+
+if (is_user_logged_in() ){
+	get_header('user');
+}
+else {
+	get_header();
+}
+
+$posts = query_posts($query_string.'&cat=7'); ?>
+<div class="container">
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class="row">
+				<div class="col-md-12 news">
+					
+						<a class="box-title" href="<?php the_permalink(); ?>" ><?php echo get_the_title(); ?></a>
+					
+				</div>
+			</div>
+			
+			<?php endwhile; else : ?>
+			
+			<?php endif; ?>
+		
+		
+	
+</div>
+
+
+

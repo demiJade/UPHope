caf201299907d5bcc9c0e940a2c2c0100bb9d8af
demijade <sy_judy@yahoo.com>
2016-08-20 15:42:04 +0800

    edit modal for users

--- user-meta/admin-user-dashboard.php
+++ user-meta/admin-user-dashboard.php
@@ -30,8 +30,51 @@ get_header('admin');
 			<td><?php if ($user->formations_seminar != '') {echo $user->project_involvement;} else {echo 0;} ?></td>
 			<td><?php if ($user->child_life_party != '') {echo $user->project_involvement;} else {echo 0;} ?></td>
 			<td>
-				<button>Edit</button>
-				<?php if ($user->project_involvement == '' && $user->hospital_visits == '' && $user->formations_seminar == '') {?><button id="delete" onclick="deleteUser(<?php echo $user->ID ?>)">Delete</button><?php } ?>
+				<button class="btn" data-toggle="modal" data-target="#editUser<?php echo $user->ID ?>">Edit</button>
+				<?php if ($user->project_involvement == '' && $user->hospital_visits == '' && $user->formations_seminar == '') {?><button id="delete" class="btn" onclick="if (confirm('Are you sure you want to delete <?php echo $user->user_firstname.' '.$user->user_lastname ?>?')){deleteUser(<?php echo $user->ID ?>)}">Delete</button><?php } ?>
+								<!-- Modal -->
+				<div class="modal fade" id="editUser<?php echo $user->ID ?>" tabindex="-1" role="dialog" aria-labelledby="User<?php echo $user->ID ?>">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				        <h4 class="modal-title" id="myModalLabel">Edit User: <?php echo $user->user_firstname.' '.$user->user_lastname; ?></h4>
+				      </div>
+				      <div class="modal-body">
+				        <form class="form-horizontal">
+				        	<div class="form-group">
+				        		<label class="control-label col-sm-5" for="project-involvement">Projects Involvement: </label>
+				        		<div class="col-sm-5">
+				        			<input type="text" class="form-control" id="project-involvement">
+				        		</div>
+				        	</div>
+				        	<div class="form-group">
+				        		<label class="control-label col-sm-5" for="hospital-visits">Hospital Visits: </label>
+				        		<div class="col-sm-5">
+				        			<input type="text" class="form-control" id="hospital-visits">
+				        		</div>
+				        	</div>
+				        	<div class="form-group">
+				        		<label class="control-label col-sm-5">Formations Seminar: </label>
+				        		<div class="col-sm-5">
+				        			<input type="text" class="form-control" id="formations-seminar">
+				        		</div>
+				        	</div>
+				        	<div class="form-group">
+				        		<label class="control-label col-sm-5">Child Life Party: </label>
+				        		<div class="col-sm-5">
+				        			<input type="text" class="form-control" id="child-life-party">
+				        		</div>
+				        	</div>
+				        </form>
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        <button type="button" class="btn btn-primary">Save changes</button>
+				      </div>
+				    </div>
+				  </div>
+				</div>
 			</td>
 		</tr>
 	<?php } ?>
